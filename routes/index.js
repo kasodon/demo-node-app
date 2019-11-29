f0718a2af0857b5264715d35ae7d8f2473149db1
Edewor Onyedika <edwordkaso100@gmail.com>
2019-11-29 21:03:56 +0100

    finished app

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,21 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
+const auth = require('http-auth');
 const { body, validationResult } = require('express-validator/check');
 
 const router = express.Router();
 const Registration = mongoose.model('Registration');
-/*router.get('/', (req, res) => {  
-    res.send('It works!');
-});*/
-
-
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
 
 router.get('/', (req, res) => {
-    res.render('form', { title: 'Registration form' });
-  });
+  res.render('form', { title: 'Registration form' });
+});
 
-  /*router.post('/', (req, res) => {
-    console.log(req.body);
-    res.render('form', { title: 'Registration form' });
-  });  */
-
-  router.post('/',
+router.post(
+  '/',
   [
     body('name')
       .isLength({ min: 1 })
@@ -32,10 +28,10 @@ router.get('/', (req, res) => {
     const errors = validationResult(req);
 
     if (errors.isEmpty()) {
-        const registration = new Registration(req.body);
-        registration.save()
-          .then(() => { res.send('Thank you for your registration!'); })
-          .catch(() => { res.send('Sorry! Something went wrong.'); });
+      const registration = new Registration(req.body);
+      registration.save()
+        .then(() => { res.send('Thank you for your registration!'); })
+        .catch(() => { res.send('Sorry! Something went wrong.'); });
     } else {
       res.render('form', {
         title: 'Registration form',
@@ -46,14 +42,12 @@ router.get('/', (req, res) => {
   }
 );
 
-router.get('/registrations', (req, res) => {
-    Registration.find()
-      .then((registrations) => {
-        res.render('index', { title: 'Listing registrations', registrations });
-      })
-      .catch(() => { res.send('Sorry! Something went wrong.'); });
-  });
-  
-    
-  
+router.get('/registrations', auth.connect(basic), (req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('index', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+});
+
 module.exports = router;
